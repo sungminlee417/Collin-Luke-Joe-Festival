f36f4455ea4909aae428d9921e4df0113adb2452
hannahlee2 <hanlee021@gmail.com>
2023-02-23 22:24:54 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Performances from "./components/Performances/Performances";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <LandingPage/>
       <Performances />
       <Contact />
+      <Footer />
     </div>
   );
 }
