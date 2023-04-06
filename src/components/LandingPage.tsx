9d9609a9b9b5f316c4803b286090d0a3efe206eb
Sungmin Lee <sungminlee417@gmail.com>
2023-04-06 12:14:47 -0700

    added media queries to landing page and navigation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -11,7 +11,6 @@ const LandingPage = () => {
         <div className="absolute flex flex-col justify-center lg:text-10xl text-[#353535] h-full font-playfair text-center md:text-9xl text-8xl">
           <div>Long Lake</div>
           <div> Music Festival</div>
-<<<<<<< HEAD
           <div className="lg:text-5xl md:text-3xl text-2xl text-white italic p-10 font-source">
             <strong>"Off The Dock Chamber Festival</strong> aims to foster an
             invigorating concert-going experience by embracing the energy of new
@@ -20,15 +19,6 @@ const LandingPage = () => {
             is an inspiring modern platform for emerging artists on the East
             Coast."
           </div>
-=======
-          <div className="text-4xl text-white italic p-10 font-source">
-          <strong>"Off The Dock Chamber Festival</strong> aims to foster an
-          invigorating concert-going experience by embracing the energy of new
-          up-and-coming artists. With the promise of providing a gateway to
-          connect with a deeply engaged classical music loving community, OTD is
-          an inspiring modern platform for emerging artists on the East Coast."
-        </div>
->>>>>>> refs/remotes/origin/main
         </div>
       </div>
     </>
