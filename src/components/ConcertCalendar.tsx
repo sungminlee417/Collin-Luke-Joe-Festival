4dec424c76b979ad32401a3668c45161d960322c
Sungmin Lee <sungminlee417@gmail.com>
2023-04-18 10:35:01 -0700

    merged

--- src/components/ConcertCalendar.tsx
+++ src/components/ConcertCalendar.tsx
@@ -1,42 +1,61 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const events = [
   {
     title: "Vocalists & Soloists",
     start: new Date("July 6, 2023 20:00:00"),
     end: new Date("July 6, 2023 21:30:00"),
+    url: "https://www.eventbrite.com/e/619799867087",
+  },
+  {
+    title: "Original Compositions",
+    start: new Date("July 7, 2023 13:00:00"),
+    end: new Date("July 7, 2023 14:30:00"),
+    url: "https://www.eventbrite.com/e/619826988207",
+  },
+  {
+    title: "Baroque Concert",
+    start: new Date("July 7, 2023 20:00:00"),
+    url: "https://www.eventbrite.com/e/619829405437",
+  },
+  {
+    title: "Jazz and Show Tunes",
+    start: new Date("July 8, 2023 13:00:00"),
+    end: new Date("July 8, 2023 14:30:00"),
+    url: "https://www.eventbrite.com/e/619830759487",
+  },
+  {
+    title: "Folk and Fiddle",
+    start: new Date("July 8, 2023 20:00:00"),
+    url: "https://www.eventbrite.com/e/619832053357",
+  },
+  {
+    title: "Potluck",
+    start: new Date("July 9, 2023 16:00:00"),
+    end: new Date("July 9, 2023 19:00:00"),
+  },
+  {
+    title: "Duo Concert",
+    start: new Date("July 9, 2023 19:00:00"),
   },
 ];
 
 const ConcertCalendar = () => {
+  const handleDateClick = () => {};
+
   return (
-    <div className="px-96 py-20">
+    <div className=" px-80 py-20">
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        weekends={false}
         events={events}
-        eventContent={renderEventContent}
+        eventClick={(arg) => console.log(arg.event.title)}
+        // eventContent={renderEventContent}
       />
     </div>
   );
 };
-
-interface EventInfo {
-  event: {
-    title: string;
-  };
-  timeText: string;
-}
-
-function renderEventContent(eventInfo: EventInfo) {
-  return (
-    <div className="flex flex-col">
-      <p>{eventInfo.event.title}</p>
-      <p>{eventInfo.timeText}</p>
-    </div>
-  );
-}
 
 export default ConcertCalendar;
